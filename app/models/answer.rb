1846ba7cb6e53be07cce703133473e8c6da184cb
Harrison Muskat <harrisonmuskat@gmail.com>
2017-04-10 13:06:08 -0400

    Created table migrations and models

---
+++ app/models/answer.rb
@@ -0,0 +1,7 @@
+class Answer < ApplicationRecord
+  belongs_to :question
+  belongs_to :user
+
+  validates :description, presence: true, length: { minimum: 50 }
+  validates :best, inclusion: { in: [true, false] }
+end
---
+++ app/models/question.rb
@@ -0,0 +1,7 @@
+class Question < ApplicationRecord
+  belongs_to :user
+  has_many :answers
+
+  validates :title, presence: true, length: { minimum: 40 }
+  validates :description, presence: true, length: { minimum: 150 }
+end
